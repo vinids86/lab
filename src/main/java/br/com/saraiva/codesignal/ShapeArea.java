e9ffa3331d699f23df504d1498a014be85d1eab4
Vinícius D Saraiva <viniciusdsaraiva@gmail.com>
2018-09-24 10:41:18 -0300

    shape area

---
+++ src/main/java/br/com/saraiva/codesignal/ShapeArea.java
@@ -0,0 +1,11 @@
+package br.com.saraiva.codesignal;
+
+/**
+ * Created by sara on set, 2018
+ */
+class ShapeArea {
+
+	int execute(int n) {
+		return 5;
+	}
+}
---
+++ src/test/java/br/com/saraiva/codesignal/ShapeAreaTest.java
@@ -0,0 +1,20 @@
+package br.com.saraiva.codesignal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by sara on set, 2018
+ */
+class ShapeAreaTest {
+
+	@Test
+	void execute() {
+		final int input = 2;
+		final int actual = new ShapeArea().execute(input);
+		final int expected = 5;
+
+		assertEquals(expected, actual);
+	}
+}
